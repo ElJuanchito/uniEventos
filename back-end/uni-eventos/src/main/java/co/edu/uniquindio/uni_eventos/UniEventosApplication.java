92dee0106a54ae14da383b29a0fd5551678ad5e3
ElJuanchito <perdomocardenas18@gmail.com>
2024-08-28 01:13:21 -0500

    Iniciando el backend

---
+++ back-end/uni-eventos/src/main/java/co/edu/uniquindio/uni_eventos/UniEventosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.uni_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniEventosApplication.class, args);
+    }
+
+}
---
+++ back-end/uni-eventos/src/test/java/co/edu/uniquindio/uni_eventos/UniEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.uni_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
